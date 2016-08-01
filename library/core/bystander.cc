f449294a4f0084e9e4fe58f0dd5ce041482b03ef
Noah Oblath <nsoblath@gmail.com>
2016-08-01 14:40:55 -0700

    Added bystander class

---
+++ library/core/bystander.cc
@@ -0,0 +1,23 @@
+/*
+ * bystander.cc
+ *
+ *  Created on: Aug 1, 2016
+ *      Author: obla999
+ */
+
+#include "bystander.hh"
+
+namespace midge
+{
+
+    bystander::bystander() :
+            node(),
+            instructable()
+    {
+    }
+
+    bystander::~bystander()
+    {
+    }
+
+} /* namespace midge */
