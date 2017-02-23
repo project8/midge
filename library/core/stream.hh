e450d50bcc3e0678d4a49b5f9fdca7737f1f66ff
Noah Oblath <nsoblath@gmail.com>
2017-02-22 18:09:01 -0800

    Added stream::s_skip

--- library/core/stream.hh
+++ library/core/stream.hh
@@ -16,6 +16,7 @@ namespace midge
             static const enum_t s_start;
             static const enum_t s_run;
             static const enum_t s_stop;
+            static const enum_t s_skip;
             static const enum_t s_exit;
             static const enum_t s_error;
 
